98228aae43ec4442405b2135ecdf931963d956f3
zhenglier <45163453+zhenglier@users.noreply.github.com>
2018-12-16 19:23:20 +0800

    Add files via upload

--- mystl.cpp
+++ mystl.cpp
@@ -176,6 +176,79 @@ namespace mystl{
 		#undef int
 	}
 	//sort end 
+	
+	//iostream begin
+	//Build. --2018.12.16
+	//Support int,long long,char. --2018.12.16
+	struct istream{}cin;
+	struct ostream{}cout;
+	#define gc(x) (x=getchar())
+	#define pc(x) putchar(x)
+	#define isdigit(c) ('0'<=(c)&&(c)<='9')
+	#define ok(x) ((x)!=' '&&(x)!='\r'&&(x)!='\n')
+	//IN:
+	//int
+	istream& operator >>(const istream& in,int& val){
+		int ret=0;bool f=0;char c;
+		for(gc(c);!isdigit(c);c=getchar())f|=(c=='-');
+		for(;isdigit(c);ret=(ret<<1)+(ret<<3)+c-'0',gc(c));
+		val=(f?-ret:ret);
+	}
+	//long long
+	istream& operator >>(const istream& in,long long& val){
+		long long ret=0;bool f=0;char c;
+		for(gc(c);!isdigit(c);c=getchar())f|=(c=='-');
+		for(;isdigit(c);ret=(ret<<1ll)+(ret<<3ll)+c-'0',gc(c));
+		val=(f?-ret:ret);
+	}
+	//char
+	istream& operator >>(const istream& in,char &val){
+	    gc(val);
+		while(!ok(val))gc(val);
+	}
+	
+	//OUT:
+	//endl
+	struct _endl{}endl;
+	ostream& operator <<(const ostream& out,_endl endl){
+		putchar('\n'),putchar('\r');
+	}
+	//int
+	ostream& operator <<(const ostream& out,int val){
+		char* c=new char[10];int cnt=0;
+		if(val<0)pc('-'),val=-val;
+		while(val){
+			*(c+cnt)=val%10+'0';
+			val/=10;
+			cnt++;
+		}
+		while(cnt){
+			cnt--;
+			pc(*(c+cnt));
+		}
+	}
+	//long long
+	ostream& operator <<(const ostream& out,long long val){
+		char* c=new char[220];int cnt=0;
+		if(val<0)pc('-'),val=-val;
+		while(val){
+			*(c+cnt)=val%10+'0';
+			val/=10;
+			cnt++;
+		}
+		while(cnt){
+			cnt--;
+			pc(*(c+cnt));
+		}
+	}
+	//char
+	ostream& operator <<(const ostream& out,char val){
+		pc(val);
+	}
+	#undef gc
+	#undef pc
+	#undef isdigit
+	//iostream end
 }
 int main(){
 	
